7b20be004a1849d7c9f1e80621a71eb5b6c5ebed
henrique11varela <henrique11varela@gmail.com>
2023-01-05 22:23:31 +0000

    File upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int fileSize;
+    string fileName;
+    cout << "Welcome to my dummy file maker!\n\nFile size in Mb: ";
+    cin >> fileSize;
+    cout << "File name: ";
+    cin >> fileName;
+    ofstream file;
+    file.open(fileName + ".txt");
+    for (int i = 0; i < fileSize; i++)
+    {
+        for (unsigned long long i = 0; i < 1048576; i++)
+        {
+            file << "A";
+        }
+        file << "\n";
+    }
+    file.close();
+    cout << "\nYour file is called \"" << fileName << ".txt\"\n\nThanks for using my program!";
+    cin.ignore();
+    cin.ignore();
+    return 0;
+}
